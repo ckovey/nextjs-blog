13b488a8594efc3c18f01359cbcd086e926d4140
Chris Kovey <kovey.1@osu.edu>
2024-10-08 11:42:59 -0400

    added basic pages and nav

--- pages/page.js
+++ pages/page.js
@@ -1,11 +1,11 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Page() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Some Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -20,42 +20,39 @@ export default function Home() {
         </div>
 
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          This is some other page
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam autem, cupiditate delectus dolor dolores id
+          itaque laboriosam libero maiores nisi omnis pariatur perspiciatis placeat, quod ratione sequi soluta suscipit
+          tempore.
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam autem, cupiditate delectus dolor dolores id
+          itaque laboriosam libero maiores nisi omnis pariatur perspiciatis placeat, quod ratione sequi soluta suscipit
+          tempore.
+        </p>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam autem, cupiditate delectus dolor dolores id
+          itaque laboriosam libero maiores nisi omnis pariatur perspiciatis placeat, quod ratione sequi soluta suscipit
+          tempore.
+        </p>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam autem, cupiditate delectus dolor dolores id
+          itaque laboriosam libero maiores nisi omnis pariatur perspiciatis placeat, quod ratione sequi soluta suscipit
+          tempore.
+        </p>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam autem, cupiditate delectus dolor dolores id
+          itaque laboriosam libero maiores nisi omnis pariatur perspiciatis placeat, quod ratione sequi soluta suscipit
+          tempore.
+        </p>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam autem, cupiditate delectus dolor dolores id
+          itaque laboriosam libero maiores nisi omnis pariatur perspiciatis placeat, quod ratione sequi soluta suscipit
+          tempore.
+        </p>
       </main>
 
       <footer>
