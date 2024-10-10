b16dc60cb7bdd109f3cf8aaa27e1209c83c0bd82
Chris Kovey <kovey.1@osu.edu>
2024-10-10 14:54:38 -0400

    set min width on syndeca

--- components/syndeca.js
+++ components/syndeca.js
@@ -1,14 +1,14 @@
 import Script from "next/script";
-
+import styles from '../styles/Syndeca.module.css';
 export default function Syndeca() {
   return (
-    <>
-    <div id={'syndeca'}></div>
-    <Script
-      src="//viewer.syndeca.com/scripts/syndeca-embed.js"
-      data-guide-key="FB41283F-0E88-4C88-B9F7-843DC7EC2156"
-      data-log="true"
-    ></Script>
-    </>
+    <div className={styles.syndeca}>
+      <div id={'syndeca'}></div>
+      <Script
+        src="//viewer.syndeca.com/scripts/syndeca-embed.js"
+        data-guide-key="FB41283F-0E88-4C88-B9F7-843DC7EC2156"
+        data-log="true"
+      ></Script>
+    </div>
   );
 }
